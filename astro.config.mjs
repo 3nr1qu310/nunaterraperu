5e9313bd281ff5aa82d09b252e7078555d795c85
3nr1qu310 <204318@unsaac.edu.pe>
2026-05-21 18:41:46 -0500

    renderizado hibrydo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://nunaterraperu.com',
 
   output: 'static',
+
+  adapter: cloudflare(),
 
   integrations: [sitemap()],
 
